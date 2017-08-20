a081cf44973e6c30cbfd9ee605402f54b561bc0b
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-08-20 14:24:20 +0200

    Add custom JS methods for toggle and mail links

--- bootstrap/js/custom.js
+++ bootstrap/js/custom.js
@@ -1,13 +1,38 @@
 // Init
 $(document).ready(function() {
-    console.log('Ciaone');
-
     // Navbar collapse/expand
     $('nav.navbar').on('show.bs.collapse', function () {
-        console.log('Collapsing');
         $('#logo').addClass('reduced');
     });
     $('nav.navbar').on('hide.bs.collapse', function () {
         $('#logo').removeClass('reduced');
     });
+
+    // Content toggles
+    $("#page-content .toggle").each(function() {
+        // Wrap with hyperlink
+        var link = document.createElement('a');
+        link.setAttribute('href', 'javascript:void(0)');
+        $(this).wrapInner(link).append('&nbsp;▼');
+
+        // Hide next DOM element
+        $(this).next().hide();
+
+        // Add toggle handler on element
+        $(this).click(function() {
+            $(this).next().slideToggle(300);
+        });
+    });
+
+    // Init email links
+    setTimeout(function() {
+        $(".mail-link").each(function() {
+            var content = $(this).html();
+
+            var link = content.replace(/ \(at\) /gi, "@");
+            link = link.replace(/ \(dot\) /gi, ".");
+
+            $(this).html("<a href=\"mailto:" + link + "\">" + link + "</a>");
+        });
+    }, 1000);
 });
