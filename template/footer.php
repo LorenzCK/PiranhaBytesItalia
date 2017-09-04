89d5f53c97cc341a090f0a0469af949419397f69
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-09-04 15:01:01 +0200

    New footer structure

--- template/footer.php
+++ template/footer.php
@@ -5,39 +5,57 @@
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/brand-0.5.png" width="630" height="1389" alt="Piranha Bytes Italia background" srcset="<?php echo get_stylesheet_directory_uri(); ?>/resources/brand-0.5.png 315w, <?php echo get_stylesheet_directory_uri(); ?>/resources/brand-1.0.png 630w" />
       </div>
 
-    </div>
+      <footer>
+        <div class="container">
+          <div class="row hidden-xs">
+            <div class="col-sm-6">
+              Scheletro
+            </div>
 
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-6 access-links">
-            <ul>
-              <li><a href="<?php echo get_home_url(); ?>" title="Piranha Bytes Italia homepage e notizie">Homepage</a></li>
-              <li><a href="<?php echo get_term_link('gothic-saga', 'category'); ?>" title="Notizie relative alla saga di Gothic">Gothic Saga</a></li>
-              <li><a href="<?php echo get_term_link('elex', 'category'); ?>" title="Notizie relative ad Elex">Elex</a></li>
-              <li><a href="<?php echo get_term_link('risen-saga', 'category'); ?>" title="Notizie relative alla saga di Risen">Risen Saga</a></li>
-              <li><a href="<?php echo pbi_page_permalink_from_slug('staff'); ?>" title="Staff e contatti">Staff e contatti</a></li>
-              <li><a href="http://forum.multiplayer.it/forumdisplay.php?332-Gothic-Italia" title="Forum su Multiplayer.it">Forum</a></li>
-              <li class="icon-list">
-                <a href="https://www.facebook.com/groups/staffrisenitalia/" title="Iscriviti al gruppo Piranha Bytes Italia su Facebook"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/icon-facebook-black.png" alt="Facebook icon" /></a>
-                <a href="https://www.youtube.com/user/GothicRisenItalia" title="Segui Piranha Bytes Italia su Youtube"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/icon-youtube-black.png" alt="Youtube icon" /></a>
-                <a href="https://plus.google.com/+GothicRisenItalia" title="Segui Piranha Bytes Italia su Google+"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/icon-googleplus-black.png" alt="Google Plus icon" /></a>
-                <a href="<?php bloginfo('rss2_url'); ?>" title="Segui il Feed RSS di Piranha Bytes Italia"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/icon-rss-black.png" alt="RSS 2.0 Feed icon" /></a>
-              </li>
-            </ul>
+            <div class="col-sm-6">
+              Gnomo
+            </div>
           </div>
 
-          <div class="col-sm-6 copyright">
-              <p>&copy;&nbsp;Piranha Bytes Italia <?php echo date('Y'); ?>.</p>
+          <div class="row">
+            <div class="col-xs-6 visible-xs-block access-links">
+              <ul>
+                <li class="header">Navigazione</li>
+                <li><a href="<?php echo get_home_url(); ?>" title="Piranha Bytes Italia homepage e notizie">Homepage</a></li>
+                <li><a href="<?php echo pbi_page_permalink_from_slug('staff'); ?>" title="Staff e contatti">Staff e contatti</a></li>
+                <li><a href="<?php echo pbi_page_permalink_from_slug('partner-websites'); ?>" title="Web partner">Web partner</a></li>
+                <li><a href="<?php echo pbi_page_permalink_from_slug('donazione'); ?>" title="Donazioni">Donazioni</a></li>
+                <li><a href="http://forum.multiplayer.it/forumdisplay.php?332-Gothic-Italia" title="Forum su Multiplayer.it">Forum</a></li>
+              </ul>
+            </div>
 
-              <p>Tutti i contenuti relativi a Gothic®, Risen® ed Elex® sono marchi registrati e di proprietà di Pluto 13 GmbH &copy;&nbsp;1997&ndash;2017.</p>
+            <div class="col-xs-6 visible-xs-block access-links">
+              <ul>
+                <li class="header">Saghe</li>
+                <li><a href="<?php echo get_term_link('gothic-saga', 'category'); ?>" title="Notizie relative alla saga di Gothic">Gothic Saga</a></li>
+                <li><a href="<?php echo get_term_link('elex', 'category'); ?>" title="Notizie relative ad Elex">Elex</a></li>
+                <li><a href="<?php echo get_term_link('risen-saga', 'category'); ?>" title="Notizie relative alla saga di Risen">Risen Saga</a></li>
+                <li class="icon-list">
+                  <a href="https://www.facebook.com/groups/staffrisenitalia/" title="Iscriviti al gruppo Piranha Bytes Italia su Facebook"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/icon-facebook-black.png" alt="Facebook icon" /></a>
+                  <a href="https://www.youtube.com/user/GothicRisenItalia" title="Segui Piranha Bytes Italia su Youtube"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/icon-youtube-black.png" alt="Youtube icon" /></a>
+                  <a href="https://plus.google.com/+GothicRisenItalia" title="Segui Piranha Bytes Italia su Google+"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/icon-googleplus-black.png" alt="Google Plus icon" /></a>
+                  <a href="<?php bloginfo('rss2_url'); ?>" title="Segui il Feed RSS di Piranha Bytes Italia"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/icon-rss-black.png" alt="RSS 2.0 Feed icon" /></a>
+                </li>
+              </ul>
+            </div>
 
-              <p>Il contenuto di questo fansite è di proprietà dei curatori del sito, e qualsiasi utilizzo del materiale, anche parziale, deve essere prima autorizzato dallo staff. Tutti i diritti riservati. Per ulteriori informazioni contattate l'amministratore del sito.
-              Tutti gli altri marchi e/o marchi registrati sono di proprietà dei legittimi detentori.</p>
+            <div class="col-xs-12 col-sm-12 copyright">
+                <p>&copy;&nbsp;Piranha Bytes Italia <?php echo date('Y'); ?>.</p>
+
+                <p>Tutti i contenuti relativi a Gothic®, Risen® ed Elex® sono marchi registrati e di proprietà di Pluto 13 GmbH &copy;&nbsp;1997&ndash;2017.</p>
+
+                <p>Il contenuto di questo fansite è di proprietà dei curatori del sito, e qualsiasi utilizzo del materiale, anche parziale, deve essere prima autorizzato dallo staff. Tutti i diritti riservati. Per ulteriori informazioni contattate l'amministratore del sito.
+                Tutti gli altri marchi e/o marchi registrati sono di proprietà dei legittimi detentori.</p>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
