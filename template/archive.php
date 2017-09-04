14e5128a896112a1c8ad169de73380b6c76523a6
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-09-04 18:17:14 +0200

    Improve category title on archive pages

--- template/archive.php
+++ template/archive.php
@@ -2,7 +2,16 @@
 get_header();
 ?>
 
-<h1><?php single_term_title('', true); ?></h1>
+<h1><?php
+$queried_object = get_queried_object();
+if($queried_object && $queried_object->taxonomy == 'category' && !$queried_object->parent) {
+    // Root category, show as "News" to avoid repetition
+    echo 'Notizie';
+}
+else {
+    single_term_title('', true);
+}
+?></h1>
 
 <hr class="top-edge" />
 
