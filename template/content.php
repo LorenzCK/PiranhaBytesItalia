328d70a2920531f10486d3c5a68602964f7af6ee
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-10-10 22:45:48 +0200

    Add edit link to posts, fixes #6

--- template/content.php
+++ template/content.php
@@ -4,8 +4,9 @@
 
 <?php if(!is_page()) : ?>
 <div class="metadata">
-    <div class="publish-date">Pubblicato il <span><?php the_time(get_option('date_format')); ?></span></div>
-    <?php the_tags('<div class="tags">Tag:&nbsp;', ', ', '</div>'); ?> 
+    <div class="publish-date">Pubblicato <span><?php the_time(get_option('date_format')); ?></span></div>
+    <?php the_tags('<div class="tags">Tag:&nbsp;', ', ', '</div>'); ?>
     <div class="comments"><?php if(!is_singular()) { echo '<a href="'; the_permalink(); echo '#comments">'; } ?><?php comments_number('Nessun commento', 'Un commento', '% commenti'); ?><?php if(!is_singular()) { echo '</a>'; } ?></div>
+    <?php edit_post_link('Modifica articolo', '<div class="edit">', '</div>'); ?>
 </div>
 <?php endif; ?>
